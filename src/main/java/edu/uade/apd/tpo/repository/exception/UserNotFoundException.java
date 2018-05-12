810dfee17c07ee22437f98f5b12ac720a6f2e979
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2018-05-12 13:33:18 -0300

    wip

---
+++ src/main/java/edu/uade/apd/tpo/repository/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package edu.uade.apd.tpo.repository.exception;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
