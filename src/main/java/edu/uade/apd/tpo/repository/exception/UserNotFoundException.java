de3d9a0a5e87908623fd55b992e32e69e66d6343
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2018-05-28 20:20:28 -0300

    remove exc

--- src/main/java/edu/uade/apd/tpo/repository/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package edu.uade.apd.tpo.repository.exception;
-
-public class UserNotFoundException extends Exception {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-
-}
