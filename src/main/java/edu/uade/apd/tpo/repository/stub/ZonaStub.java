0d729bb81cbd442536cea34c6e06f4b29eb22b76
Mariel Melaragno <mmelaragno@despegar.com>
2018-06-20 19:06:21 -0300

    cambio en zonaStub

--- src/main/java/edu/uade/apd/tpo/repository/stub/ZonaStub.java
+++ src/main/java/edu/uade/apd/tpo/repository/stub/ZonaStub.java
@@ -2,18 +2,31 @@ package edu.uade.apd.tpo.repository.stub;
 
 public enum ZonaStub {
 
-    CABA (3),
-    NORTE (6),
-    SUR (4),
-    OESTE  (5);
+	CABA(3, "Caba"), NORTE(6, "Norte"), SUR(4, "Sur"), OESTE(5, "Oeste");
 
-    private float precio;
+	private float precio;
+	private String zonaStr;
 
-    ZonaStub (float precio){
-        this.precio = precio;
-    }
+	ZonaStub(float precio, String zonaStr) {
+		this.zonaStr = zonaStr;
+		this.precio = precio;
+	}
 
-    public float getPrecio() {
-        return this.precio;
-    }
+	public float getPrecio() {
+		return this.precio;
+	}
+
+	public String getZonaStr() {
+		return zonaStr;
+	}
+
+	public static ZonaStub fromString(String zonaString) {
+		for (ZonaStub z : ZonaStub.values()) {
+			if (z.getZonaStr().equals(zonaString)) {
+				return z;
+			}
+		}
+		throw new IllegalArgumentException("Value '" + zonaString + "' not found");
+	}
+
 }
