2797ff9863fe0227c9c845a4253fab1a300db01a
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2018-07-01 18:37:03 -0300

    pedidos completos

--- src/main/java/edu/uade/apd/tpo/repository/SistemaDespachoRepository.java
+++ src/main/java/edu/uade/apd/tpo/repository/SistemaDespachoRepository.java
@@ -1,9 +1,14 @@
 package edu.uade.apd.tpo.repository;
 
+import edu.uade.apd.tpo.repository.dto.PedidoDTO;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface SistemaDespachoRepository extends Remote {
 
     void despacharPedido(Long pedidoId) throws RemoteException;
+
+    List<PedidoDTO> obtenerPedidosCompletos() throws RemoteException;
 }
--- src/main/java/edu/uade/apd/tpo/repository/delegate/DespachoDelegate.java
+++ src/main/java/edu/uade/apd/tpo/repository/delegate/DespachoDelegate.java
@@ -1,10 +1,12 @@
 package edu.uade.apd.tpo.repository.delegate;
 
 import edu.uade.apd.tpo.repository.SistemaDespachoRepository;
+import edu.uade.apd.tpo.repository.dto.PedidoDTO;
 import edu.uade.apd.tpo.repository.exception.RemoteBusinessException;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public class DespachoDelegate {
 
@@ -25,6 +27,14 @@ public class DespachoDelegate {
     public void despacharPedido(Long pedidoId) throws RemoteBusinessException {
         try {
             sistemaDespachoRepository.despacharPedido(pedidoId);
+        } catch (RemoteException re) {
+            throw new RemoteBusinessException(re.getMessage());
+        }
+    }
+
+    public List<PedidoDTO> obtenerPedidosCompletos() throws RemoteBusinessException {
+        try {
+            return sistemaDespachoRepository.obtenerPedidosCompletos();
         } catch (RemoteException re) {
             throw new RemoteBusinessException(re.getMessage());
         }
