1668d10c691564303cfa231845caa173d9124f0e
Erika Nuñez <erikannunez@gmail.com>
2018-07-01 20:35:59 -0300

    estado pedido

--- src/main/java/edu/uade/apd/tpo/repository/dto/EstadoPedidoDTO.java
+++ src/main/java/edu/uade/apd/tpo/repository/dto/EstadoPedidoDTO.java
@@ -3,13 +3,28 @@ package edu.uade.apd.tpo.repository.dto;
 import java.io.Serializable;
 
 public enum EstadoPedidoDTO implements Serializable {
-
-    GENERADO,
-    PREAPROBADO,
-    SALDO_INSUFICIENTE,
-    COMPLETO,
-    RECHAZADO,
-    A_FACTURAR,
-    PENDIENTE,
-    FACTURADO;
+	
+    GENERADO ("Generado"),
+    PREAPROBADO ("Preaprobado"),
+    SALDO_INSUFICIENTE ("Saldo insuficiente"),
+    COMPLETO ("Completo"),
+    RECHAZADO ("Rechazado"),
+    A_FACTURAR ("A facturar"),
+    PENDIENTE ("Pendiente"),
+    FACTURADO ("Facturado");
+	
+	private String name;
+	
+	EstadoPedidoDTO(){
+		
+	}
+	
+	EstadoPedidoDTO (String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
 }
