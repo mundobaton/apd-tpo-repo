478e0b87470ddcbef8e01cbb864bd9799112db95
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2018-07-01 16:30:08 -0300

    wip

--- src/main/java/edu/uade/apd/tpo/repository/SistemaDepositoRepository.java
+++ src/main/java/edu/uade/apd/tpo/repository/SistemaDepositoRepository.java
@@ -1,6 +1,7 @@
 package edu.uade.apd.tpo.repository;
 
 import edu.uade.apd.tpo.repository.dto.ArticuloDTO;
+import edu.uade.apd.tpo.repository.dto.ReposicionDTO;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -11,5 +12,9 @@ public interface SistemaDepositoRepository extends Remote {
     List<ArticuloDTO> getArticulos() throws RemoteException;
 
     ArticuloDTO findArticuloById(Long articuloId) throws RemoteException;
+
+    List<ReposicionDTO> getReposiciones() throws RemoteException;
+
+    void reponer(Long reposicionId, int cant) throws RemoteException;
 
 }
